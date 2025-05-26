ab8ea7304fada8e443cc2e4078f1ccbe3d39fb16
James Hatfield <james.hatfield17@icloud.com>
2025-05-26 10:10:02 +0100

    Created new function that counts number of words in document

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+def main():
+    book = get_book_text("./books/frankenstein.txt")
+    get_words(book)
+
 def get_book_text(filepath):
     with open(filepath) as file:
         return file.read()
     
-def main():
-    book = get_book_text("./books/frankenstein.txt")
-    print(book)
-
+def get_words(word):
+    count = word.split()
+    len_count = len(count)
+    print(f"{len_count} words found in the document.")
+          
 main()
