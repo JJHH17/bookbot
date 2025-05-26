c01afd4ceaabdb31ffdd198154a7ca222ce14519
James Hatfield <james.hatfield17@icloud.com>
2025-05-26 15:14:59 +0100

    Create function to sort dictionary by values

--- stats.py
+++ stats.py
@@ -9,10 +9,20 @@ def get_chars(word):
     words = word.lower()
     # Get individual words
     words = list(words)
-    # Loop to populate dictionary
+    # Loop to pgoldbridgeopulate dictionary
     for char in words:
         if char in frequency:
             frequency[char] += 1 
         else:
             frequency[char] = 1
     return frequency
+
+def sorted_dict(input):
+    sorted_dict = {}
+
+    for x in input.values():
+        sorted_dict.update(input)
+
+    sorted_nums = sorted(sorted_dict.items(), key=lambda x:x[1])
+    return sorted_nums
+
