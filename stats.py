ee79036b9a70a0c00ca79528c5f4644dca63be67
James Hatfield <james.hatfield17@icloud.com>
2025-05-26 10:34:13 +0100

    Create function that returns number of characters in a given string

--- stats.py
+++ stats.py
@@ -2,4 +2,20 @@ def get_words(word):
     count = word.split()
     len_count = len(count)
     print(f"{len_count} words found in the document.")
-          
+
+def get_chars(word):
+    # Creation of dictionary
+    frequency = {}
+
+    # Convert to lowercase
+    words = word.lower()
+    # Get individual words
+    words = list(words)
+
+    for char in words:
+        if char in frequency:
+            frequency[char] += 1 
+        else:
+            frequency[char] = 1
+
+    print(frequency)
