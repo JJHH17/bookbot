96d0ef87e44bd072669e324bc115fe7ea2a63cb2
James Hatfield <james.hatfield17@icloud.com>
2025-05-26 10:17:39 +0100

    Create new file, import data analysis function to it

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_words
+
 def main():
     book = get_book_text("./books/frankenstein.txt")
     get_words(book)
@@ -6,9 +8,4 @@ def get_book_text(filepath):
     with open(filepath) as file:
         return file.read()
     
-def get_words(word):
-    count = word.split()
-    len_count = len(count)
-    print(f"{len_count} words found in the document.")
-          
 main()
---
+++ stats.py
@@ -0,0 +1,5 @@
+def get_words(word):
+    count = word.split()
+    len_count = len(count)
+    print(f"{len_count} words found in the document.")
+          
